a39a7c9a67951041922958fce1f87f77716ba4ab
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-17 15:05:47 +0300

    Latest changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 document.body.style.backgroundColor = "green";
+
+let navLink = document.getElementById('nav');
+navLink.href = 'https://www.wikipedia.org';
+
+navLink.textContent = 'Navigate to Wikipedia';
+
+tit = document.querySelector("title");
+tit.textContent = "Not Today";
