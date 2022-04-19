0c93d49ad40624cdf626f07228baf36b9e616c32
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-19 10:39:41 +0300

    querySelectorAll() usage in javascript

--- access.js
+++ access.js
@@ -7,11 +7,16 @@ const demoQueryAll = document.querySelectorAll('.demo-query-all');
 demoId.style.border = "1px solid purple";
 
 for(i=0; i<demoClass.length; i++){
-    demoClass[i].style.border = "1px solid orange"
+    demoClass[i].style.border = "1px solid orange";
 }
 
 for (i=0; i<demoTag.length; i++){
-    demoTag[i].style.border = "1px solid blue"
+    demoTag[i].style.border = "1px solid blue";
 }
 
 demoQuery.style.border = "1px solid red";
+
+
+for(i=0; i<demoQueryAll.length; i++){
+    demoQueryAll[i].style.border = "1px solid green";
+}
